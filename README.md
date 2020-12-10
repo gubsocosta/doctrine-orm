5ccc01cf6846731bce55a21121bd3883142a4803
Gabriel Costa <gubsocosta@gmail.com>
2020-12-10 19:00:20 -0300

    project intial

---
+++ README.md
@@ -0,0 +1,5 @@
+# PHP and Doctrine ORM
+
+This project was created along with the course taught by the Alura website. The course teaches you how to use ORM in PHP projects.
+
+[Course Link](https://www.alura.com.br/curso-online-php-doctrine-mapeamento-objeto-relacional)
---
+++ src/Helpers/EntityManagerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Gubsocosta\Orm\Helpers;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+
+class EntityManagerFactory
+{
+    /**
+     * @return EntityManagerInterface
+     */
+    public function getEntityManager(): EntityManagerInterface
+    {
+        $rootDir = __DIR__ . '/../..';
+
+        $config = Setup::createAnnotationMetadataConfiguration([
+            $rootDir . '/src',
+            true
+        ]);
+
+        $connection = [
+            'driver'    => 'pdo_sqlite',
+            'path'      => $rootDir .  '/var/data/db.sqlite' 
+        ];
+
+        return EntityManager::create($connection, $config);
+    }
+}
